37bdd56397fd8ed3615cfd181a826a4d587b6e63
Ziyodabonu <159676375+ziyoda-art@users.noreply.github.com>
2025-06-30 16:38:41 +0300

    .

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
 // ============ TOGGLE MENU ============
 function toggleMenu() {
-    const navLinks = document.getElementById("navLinks");
-    navLinks.classList.toggle("active");
+  const navLinks = document.getElementById("navLinks");
+  navLinks.classList.toggle("active");
+}
+// ============ NAVBAR SCROLL EFFECT ============
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
   }
-  // ============ NAVBAR SCROLL EFFECT ============
-  window.addEventListener("scroll", function () {
-    const navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50) {
-      navbar.classList.add("scrolled");
-    } else {
-      navbar.classList.remove("scrolled");
+});
+
+// menu 
+document.addEventListener("DOMContentLoaded", function () {
+  const items = document.querySelectorAll(".menu-item");
+  const showMoreBtn = document.getElementById("show-more-btn");
+  let expanded = false; // состояние меню
+
+  // Сначала показываем только первые 3
+  items.forEach((item, index) => {
+    if (index > 2) {
+      item.classList.add("hidden");
     }
   });
 
-  // menu 
-  document.addEventListener("DOMContentLoaded", function () {
-    const items = document.querySelectorAll(".menu-item");
-    const showMoreBtn = document.getElementById("show-more-btn");
-    let expanded = false; // состояние меню
+  // Обработчик нажатия кнопки
+  showMoreBtn.addEventListener("click", () => {
+    expanded = !expanded;
 
-    // Сначала показываем только первые 3
     items.forEach((item, index) => {
       if (index > 2) {
-        item.classList.add("hidden");
+        item.classList.toggle("hidden", !expanded);
       }
     });
 
-    // Обработчик нажатия кнопки
-    showMoreBtn.addEventListener("click", () => {
-      expanded = !expanded;
-
-      items.forEach((item, index) => {
-        if (index > 2) {
-          item.classList.toggle("hidden", !expanded);
-        }
-      });
-
-      // Меняем текст кнопки
-      showMoreBtn.textContent = expanded ? "Yopish" : "To‘liq menyuni ko‘rish";
-    });
+    // Меняем текст кнопки
+    showMoreBtn.textContent = expanded ? "Yopish" : "To‘liq menyuni ko‘rish";
   });
+});
+
+// Скрыть прелоадер после полной загрузки
+window.addEventListener('load', () => {
+  const preloader = document.getElementById('preloader');
+  preloader.classList.add('hidden');
+});
+
